aee1d92220c4207c91cc0f62c382669758870690
Cjhightower38 <Cjhightower38@gmail.com>
2021-09-15 19:53:31 -0400

    add font sizes and various style changes

--- python_repos.py
+++ python_repos.py
@@ -42,15 +42,32 @@ for repo_dict in repo_dicts:
 '''
 Used the LS alias and set the shade to dark blue(RGB = 2 digits each 
 color) then passed the base style the alias LCS and retained the defualt
-shade and store it in the variable my_style. Next using the Bar() method
-passing my_style, setting the rotation of the chart to 45 and removing
-the chart legend by setting to False. Finally adding the title and the 
-names of each project to the x-axis.
+shade and store it in the variable my_style. Using the style objects in
+Pygal set the title, label, and major label which represent only the 
+increments of 5k mark offs. Next creat a instance of Pygal's Config
+class setting attributes for label rotation, show legend, truncate,
+y guide, and the width. As before rotation and legend remain the same
+adding truncate to shorten the project names to 15 character unless the
+cursormhovers over. Setting y guide lines to False removes them just as
+setting legend to False removes the charts legend and finally adding
+a width of 1000 better uses the screen real estate. Passing pygal.Bar()
+the my_config as the first argument allows pygal to implement personal
+changes above Pygal's style objects.
 '''
 	
 my_style = LS('#333366', base_style = LCS)
-chart = pygal.Bar(stlye = my_style, x_label_rotation = 45,
-    show_legend = False)
+my_style.title_font_size = 24
+my_style.label_font_size = 14
+my_style.major_label_font_size = 18
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legend = False
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style = my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
